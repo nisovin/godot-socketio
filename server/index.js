fc50b1dbb977b9993dcb4f653b8561df04801b14
nisovin <github@nisovin.com>
2023-07-08 09:05:22 -0600

    Add test socket.io server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat', (msg) => {
+	  console.log('msg: ' + msg);
+	  socket.emit('chat', 'reply1');
+  });
+});
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
